e16a51174726b2fd3dfadaa839a0f8c7c8d1d90c
Kuai-mare <orionkmc@gmail.com>
2017-06-03 13:01:24 -0400

    evaluando si la palabra es un palindromo

---
+++ palindromo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def run():
+    word = str(raw_input('Escribe una palabra: '))
+    if(word == word[::-1]):
+        print('La palabra es un palindromo')
+    else:
+        print('La palabra NO es un palindromo')
+
+if __name__ == '__main__':
+    run()
