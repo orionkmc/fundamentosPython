158c27722e802f454201ac0f91b4dd8a791db6b3
Kuai-mare <orionkmc@gmail.com>
2017-06-03 13:19:53 -0400

    utilizando listas para calcular el promedio de la temperatura de 7 dias

---
+++ average_temps.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+def average_temps(temps):
+    sum_of_temps = 0
+    for temp in temps:
+        sum_of_temps += float(temp)
+
+    return sum_of_temps / len(temps)
+
+def run():
+    temps = [21, 24, 24, 22, 20, 23, 24]
+
+    average = average_temps(temps)
+    print("La temperatura promedio es: {}".format(average))
+
+
+if __name__ == '__main__':
+    run()
