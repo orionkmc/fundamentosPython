4f80f7d98d8728e9a7fce667d418626bf6d01f47
Kuai-mare <orionkmc@gmail.com>
2017-06-03 20:33:29 -0400

    haciendo juego del ahorcado

---
+++ ahorcados.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import random
+
+IMAGES = [ '''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+        |
+        |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+    |   |
+        |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+   /|   |
+        |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+   /|\  |
+        |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+   /|\  |
+    |   |
+        |
+        =========''','''
+    +---+
+    |   |
+    0   |
+   /|\  |
+    |   |
+   /    |
+        =========''','''
+    +---+
+    |   |
+    0   |
+   /|\  |
+    |   |
+   / \  |
+        =========''',
+
+]
+WORDS = [
+    'lavadora',
+    'teclado',
+    'leche',
+    'cocina',
+    'televisor',
+    'mesa',
+    'sofa',
+]
+
+def random_word():
+    idx = random.randint(0, len(WORDS) - 1)
+    return WORDS[idx]
+
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('--- * --- * --- * --- * --- * ---')
+
+def run():
+    word = random_word()
+    hidden_word = ['-'] * len(word)
+    tries = 0
+
+    while True:
+        display_board(hidden_word, tries)
+        current_letter = str(raw_input('Escoge una letra: '))
+
+        letter_indexes = []
+        for idx in range(len(word)):
+            if word[idx] == current_letter:
+                letter_indexes.append(idx)
+        if len(letter_indexes) == 0:
+            tries += 1
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print('¡Has perdido! la palabra correcta era {}.'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+            letter_indexes = []
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            break
+        
+if __name__ == '__main__':
+    print('B I E N V E N I D O S  A  A H O R C A D O S')
+    run()
